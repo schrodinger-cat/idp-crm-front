2adfba243128d1f3bb862d8a1f7813e845e01484
schrodingercat <messerfurfraumuller@gmail.com>
2019-02-22 17:56:03 +0400

    Updates

---
+++ precache-manifest.53d498e43b9300fab191f6ba6541eb2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d8337855428e65b693c8",
+    "url": "/static/js/main.d8337855.chunk.js"
+  },
+  {
+    "revision": "9348c8028c9aa1185b0f",
+    "url": "/static/js/2.9348c802.chunk.js"
+  },
+  {
+    "revision": "d8337855428e65b693c8",
+    "url": "/static/css/main.5f3d5606.chunk.css"
+  },
+  {
+    "revision": "9348c8028c9aa1185b0f",
+    "url": "/static/css/2.8503c0a4.chunk.css"
+  },
+  {
+    "revision": "12111e8d42f7296e6bcb3506c278b8f6",
+    "url": "/index.html"
+  }
+];
