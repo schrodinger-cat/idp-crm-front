e45418cf90cdb211301394aaa3194325d73b03a3
schrodingercat <messerfurfraumuller@gmail.com>
2019-02-25 11:24:57 +0400

    Updates

---
+++ precache-manifest.0a683ad1f05f146c44bb3164fdaa5a59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "901075d06b397f82bf1f",
+    "url": "/idp-crm-front/static/js/runtime~main.901075d0.js"
+  },
+  {
+    "revision": "390277ace2362dc5348e",
+    "url": "/idp-crm-front/static/js/main.390277ac.chunk.js"
+  },
+  {
+    "revision": "3fbd50e563a6221e774a",
+    "url": "/idp-crm-front/static/js/2.3fbd50e5.chunk.js"
+  },
+  {
+    "revision": "390277ace2362dc5348e",
+    "url": "/idp-crm-front/static/css/main.5f3d5606.chunk.css"
+  },
+  {
+    "revision": "3fbd50e563a6221e774a",
+    "url": "/idp-crm-front/static/css/2.8503c0a4.chunk.css"
+  },
+  {
+    "revision": "ae5470bf96540c90c35e72e73ffbc67d",
+    "url": "/idp-crm-front/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53d498e43b9300fab191f6ba6541eb2a.js"
+  "/idp-crm-front/precache-manifest.0a683ad1f05f146c44bb3164fdaa5a59.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/idp-crm-front/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
